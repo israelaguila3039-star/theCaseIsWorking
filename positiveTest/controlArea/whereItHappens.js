cf19013e315666b4e814e1e810e72ed14321b0d6
israelaguila3039-star <israel.aguila3039@stu.mtec.edu>
2026-03-23 08:13:40 -0600

    positives I think I'm done

--- positiveTest/controlArea/whereItHappens.js
+++ positiveTest/controlArea/whereItHappens.js
@@ -1,31 +1,35 @@
 import { expect } from '@wdio/globals'
 import { $ } from '@wdio/globals'
 import thePlaceILogIn from '../stuff/loggingIn.js'
-//inporting the class 
 
 
-async function theTestIng(username, password, passSelector) {    
 //This function can control what is inputted for the password, username, and the selector to determine if the test has passed.
-//This is due to neeeding to run multiple tests where
-
+//This is due to neeeding to run multiple tests where the username and what determines if they pass is different.
+//Added a means to change the string it is looking for
+async function theTestIng(username, password, passSelector, passText) {    
+    
     describe('My Login application', () => {
         it('should login with valid credentials', async () => {
             await thePlaceILogIn.open()
 
-            await thePlaceILogIn.login(username, password)
-            await expect($(passSelector)).toBeExisting()
+            await thePlaceILogIn.login(username, password)            
+            await expect($(passSelector)).toBeExisting();
             await expect($(passSelector)).toHaveText(
-                expect.stringContaining('Swag Labs'))
+                expect.stringContaining(passText));
+            
         })
     })
 
 }
-//positive tests
-theTestIng("standard_user", "secret_sauce", '[class="app_logo"]');
-theTestIng();
-theTestIng("problem_user", "secret_sauce", '[class="app_logo"]');
-theTestIng();
-theTestIng("error_user", "secret_sauce", '[class="app_logo"]');
-theTestIng("visual_user", "secret_sauce", '[class="app_logo"]');
+
+
+//Positive Tests:
+
+theTestIng("standard_user", "secret_sauce", '[class="app_logo"]','Swag Labs');
+theTestIng("locked_out_user", "secret_sauce", '[class="error-message-container error"]','Epic sadface: Sorry, this user has been locked out.');
+theTestIng("problem_user", "secret_sauce", '[class="app_logo"]', 'Swag Labs');
+theTestIng("performance_glitch_user", "secret_sauce", '[class="app_logo"]', 'Swag Labs');
+theTestIng("error_user", "secret_sauce", '[class="app_logo"]','Swag Labs');
+theTestIng("visual_user", "secret_sauce", '[class="app_logo"]','Swag Labs');
 
 //Negative tests
